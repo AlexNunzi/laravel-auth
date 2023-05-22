6809acb8db875cc0ed2c6327994c2073d3e92250
Alex Nunzi <alex.nunzi@hotmail.it>
2023-05-22 18:24:26 +0200

    Compilata vista index di projects per mostrare la tabella contenente gli elementi presenti nella tabella projects del database.

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,4 +1,29 @@
 @extends('layouts.admin')
 
 @section('content')
+    <table class="table table-stripped">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Data inizio progetto</th>
+                <th scope="col">Data fine progetto</th>
+                <th scope="col">Descrizione</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($projects as $project)
+                <tr>
+                    <td>{{ $project->id }}</td>
+                    <td>{{ $project->title }}</td>
+                    <td>{{ $project->start_date }}</td>
+                    <td>{{ $project->end_date }}</td>
+                    <td>{{ $project->description }}</td>
+                    <td>
+                        <a class="btn btn-primary" href="{{ route('admin.projects.show', $project->id) }}">Info</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
